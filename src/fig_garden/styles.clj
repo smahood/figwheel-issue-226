a2c1fb182d5d25b51e3aa9c00fd80c171e1d091a
Shaun Mahood <smahood@gmail.com>
2015-08-31 14:16:51 -0600

    Initial Commit

---
+++ src/fig_garden/styles.clj
@@ -0,0 +1,14 @@
+(ns fig-garden.styles
+  (:require [garden.def :refer [defstyles]]))
+
+(defstyles
+  screen-stylesheet
+  [:body {:background-color "red"}])
+
+;;(defstyles 
+;;print-stylesheet
+;;[:h2 {:background-color "blue"}]) 
+  
+  
+ 
+ 
---
+++ src/fig_garden/styles2.clj
@@ -0,0 +1,14 @@
+(ns fig-garden.styles2
+  (:require [garden.def :refer [defstyles]]))
+
+;;(defstyles
+;;  screen-stylesheet
+;;  [:body {:background-color "red"}])
+
+(defstyles 
+print-stylesheet
+[:#app {:background-color "blue"}])
+
+
+
+ 
